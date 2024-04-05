0cbdce52050c504b8a67e427f11536dc13370f21
markusgemeinder <190774@gmx.de>
2024-04-05 08:04:29 +0200

    Next.js 14 Tutorial 41 Headers in Route Handlers (see example and comments /profile/api/route.ts)

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,3 +1,23 @@
-export async function GET() {
-  return new Response('Profile API data');
+// variant 1
+import { type NextRequest } from 'next/server';
+
+// variant 2
+import { headers } from 'next/headers';
+
+export async function GET(request: NextRequest) {
+  // variant 1
+  const requestHeaders = new Headers(request.headers);
+  console.log(requestHeaders.get('Authorization'));
+
+  // variant 2
+  const headerList = headers();
+  console.log(headerList.get('Authorization'));
+
+  // after sending Thunder Client request console.log "Bearer 12345" will be displayed in Terminal
+
+  // headers returned from the GET function are "read only"
+
+  return new Response('<h1>Profile API data</h1>', { headers: { 'Content-Type': 'text/html' } });
 }
+
+// return new Response('<h1>Profile API data</h1>') without headers information displays plain text
