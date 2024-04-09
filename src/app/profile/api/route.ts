f4da9c35936b768d46ab4d98f5e32eb16f0c6f51
markusgemeinder <190774@gmx.de>
2024-04-09 14:23:34 +0200

    Next.js 14 Tutorial 42 Cookies in Route Handlers

--- src/app/profile/api/route.ts
+++ src/app/profile/api/route.ts
@@ -1,23 +1,25 @@
-// variant 1
-import { type NextRequest } from 'next/server';
-
-// variant 2
-import { headers } from 'next/headers';
+import { type NextRequest } from 'next/server'; // header option 1
+import { headers, cookies } from 'next/headers'; // header and cookies option 2
 
 export async function GET(request: NextRequest) {
-  // variant 1
-  const requestHeaders = new Headers(request.headers);
-  console.log(requestHeaders.get('Authorization'));
+  const requestHeaders = new Headers(request.headers); // header option 1
+  const headerList = headers(); // header option 2
 
-  // variant 2
-  const headerList = headers();
-  console.log(headerList.get('Authorization'));
+  const theme = request.cookies.get('theme'); // cookies option 1
+  cookies().set('resultsPerPage', '20'); // cookies option 2
+
+  console.log(requestHeaders.get('Authorization')); // header option 1
+  console.log(headerList.get('Authorization')); // header option 2
+
+  console.log(theme); // cookies option 1
+  console.log(cookies().get('resultsPerPage')); // cookies option 2
 
   // after sending Thunder Client request console.log "Bearer 12345" will be displayed in Terminal
-
   // headers returned from the GET function are "read only"
 
-  return new Response('<h1>Profile API data</h1>', { headers: { 'Content-Type': 'text/html' } });
+  return new Response('<h1>Profile API data</h1>', {
+    headers: { 'Content-Type': 'text/html', 'Set-Cookie': 'theme=dark' },
+  });
 }
 
 // return new Response('<h1>Profile API data</h1>') without headers information displays plain text
